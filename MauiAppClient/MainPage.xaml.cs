6521998b0ece0fd7fe13d92bbd49342131a6c60c
Gerald Versluis <gerald.versluis@microsoft.com>
2023-05-24 14:29:36 +0200

    Initial commit

---
+++ MauiAppClient/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiAppClient
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient _httpClient = new();
+        private const string BaseAddress = "https://0x52nz4m-5015.euw.devtunnels.ms";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var resultJson = await _httpClient.GetStringAsync($"{BaseAddress}/WeatherForecast/");
+
+            weatherResult.Text = resultJson;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MauiDevTunnelsSample
+ Sample code to demonstrate how to work with Dev Tunnels and .NET MAUI and ASP.NET
